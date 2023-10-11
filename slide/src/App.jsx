92da255faa4f03d6d0b643502ca5bb213cb60499
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-11 17:25:07 -0300

    Update App.jsx

--- slide/src/App.jsx
+++ slide/src/App.jsx
@@ -1,5 +1,12 @@
 import './App.css'
 
+const data = [
+  { id: '1', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/08/fullstack-blog.png'},
+  {id: '2', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/08/home.png'},
+  {id: '3', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/03/Frame-321.png'},
+  {id: '4', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/01/'},
+]
+
 import {Swiper, SwiperSlide} from 'swiper/react'
 
 function App() {
@@ -11,7 +18,7 @@ function App() {
       <Swiper>
         {data.map((item) => (
           <SwiperSlide>
-          
+            <img src={item.image} alt="slider" />
           </SwiperSlide>
         ))}
       </Swiper>
