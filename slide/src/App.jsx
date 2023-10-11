7ec8f71b084d2fceb0c2eb3328c3d83045492b9f
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-11 17:31:11 -0300

    ....

--- slide/src/App.jsx
+++ slide/src/App.jsx
@@ -2,9 +2,9 @@ import './App.css'
 
 const data = [
   { id: '1', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/08/fullstack-blog.png'},
-  {id: '2', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/08/home.png'},
-  {id: '3', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/03/Frame-321.png'},
-  {id: '4', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/01/'},
+  { id: '2', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/08/home.png'},
+  { id: '3', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/03/Frame-321.png'},
+  { id: '4', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/01/thumb-1.png'},
 ]
 
 import {Swiper, SwiperSlide} from 'swiper/react'
@@ -15,10 +15,18 @@ function App() {
     <div className="container">
       <h1 className="title">Slider com React JS</h1>
 
-      <Swiper>
+      <Swiper
+       slidesPerView={1}
+       pagination={{clickable: true}}
+       navigation
+      >
         {data.map((item) => (
-          <SwiperSlide>
-            <img src={item.image} alt="slider" />
+          <SwiperSlide key={item.id}>
+            <img 
+            src={item.image} 
+            alt="slider" 
+            className="slide-item"
+            />
           </SwiperSlide>
         ))}
       </Swiper>
