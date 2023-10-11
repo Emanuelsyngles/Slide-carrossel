1d4b17f9848df9b2cf3f5cc654cc980b4bd1d184
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-11 17:05:38 -0300

    ...

--- slide/src/App.jsx
+++ slide/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
 
+import {Swiper, SwiperSlide} from 'swiper/react'
+
 function App() {
 
   return (
     <div className="container">
       <h1 className="title">Slider com React JS</h1>
+
+      <Swiper>
+        {data.map((item) => (
+          <SwiperSlide>
+          
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   )
 }
